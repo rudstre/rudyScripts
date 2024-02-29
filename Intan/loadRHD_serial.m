52b05c29133696ad4b60e15ebf2bddf233c7382a
rudygb <gelb-bicknell@fas.harvard.edu>
2024-02-29 12:50:19 -0500

    Updated intan import files

--- Intan/loadRHD_serial.m
+++ Intan/loadRHD_serial.m
@@ -1,43 +1,87 @@
-fs = 30000;
+%% Set data to acquire
 
-t_start = 24; % starts at midnight
-t_len = 4; % ends at 4am
+fs = 30000; %30kHz
 
-read_length = t_len * 3600; % duration in seconds 
+t_start = 18; % start acquisition at this time (18:00 hours)
+t_len = 24; % total length to acquire in hours
+
 t_offset = t_start - 14; % file starts at ~14:00
 offset = t_offset * fs * 3600; % converts to sample offset
 
-read_length_s = read_length*fs; % in samples
+read_length = t_len * 3600; % duration in seconds
+read_length_s = read_length * fs; % in samples
 
-chunkSize = 60; 
-chunkSize_s = chunkSize*fs;
+chunkSize = 240; % read in 4 minute chunks
+chunkSize_s = chunkSize * fs; % convert to sample length
 
-shifts = 0:chunkSize_s:read_length_s-chunkSize_s; shifts(1) = -1;
-shifts = shifts + offset;
+shifts = 0 : chunkSize_s : read_length_s-chunkSize_s; % calculate offsets for each chunk
+shifts = shifts + offset; % add in the constant offset to start at the right time
 
-% [name,path] = uigetfile('*.*');
-% filename = fullfile(path,name);
+filename = 'Z:\Lab\forRudy\637181493672024509.rhd';
 
-filename = '/Volumes/olveczky_lab/Lab/forRudy/637181493672024509.rhd';
-
-acc_pow = [];
-ephys = [];
-t = 60; % guess for time to acquire chunk
-saveEvery = 5;
-for i = 1:length(shifts)
-    clc
-    fprintf('Loading chunk %d of %d. \nEstimated time remaining: %.1f minutes',...
-        i, length(shifts), mean(t) * (length(shifts) - i + 1) / 60)
-    tic
-    [pow,ephys_dec] = readRHDandDownsample(filename,shifts(i),chunkSize_s);
-    t(i) = toc;
-    data.acc_pow = [data.acc_pow;pow];
-    data.ephys = [data.ephys, ephys_dec];
-    if mod(i,saveEvery) == 0
-        clc
-        fprintf('Saving data at shift %d\n',i)
-        data.lastSave = i;
-        save('rhdData.mat','data');
-    end
+if ~exist('data','var')
+    data = struct('acc',[],'ephys',[],'dio',[]);
 end
 
+acc = [];
+ephys = [];
+dio = [];
+
+t = 70; % initial guess for time to acquire chunk
+saveEvery = 5; % save data every x chunks
+
+%% Acquire data
+for chk = 16:length(shifts)
+    
+    % print out how far we are
+    clc
+    fprintf('Loading chunk %d of %d. \nEstimated time remaining: %.1f minutes',...
+        chk, length(shifts), mean(t(t~=0)) * (length(shifts) - chk + 1) / 60)
+    
+    % collect current chunk
+    tic
+    [acc,ephys,dio] = readRHDandDownsample(filename,shifts(chk),chunkSize_s);
+    t(chk) = toc;
+    
+    % concatenate new chunk and old data
+    data.acc = [data.acc; acc];
+    data.ephys = [data.ephys, ephys];
+    data.dio = [data.dio, dio];
+    
+    % save data every x times
+    if mod(chk,saveEvery) == 0
+        clc
+        fprintf('Saving data at shift %d\n',chk)
+        data.lastSave = chk;
+        save('rhdData_24.mat','data','-v7.3');
+    end
+    
+end
+
+%% Calculate LFP
+
+fs_ds = fs/100; % downsampled sampling rate is fs/100
+step_s = 1 * fs_ds; % window steps in 1 second increments
+win_s = 5 * fs_ds; % window length is 5 seconds (80% overlap between windows)
+
+for ch = 1:size(data.ephys,1)
+    clc
+    fprintf('Calculating LFP for channel %d of %d', ch, size(data.ephys,1));
+    [~, freqs, t, pwr(:,:,ch)] = spectrogram(data.ephys(ch,:), ...
+        win_s, win_s - step_s, [], fs_ds, 'yaxis');
+end
+
+pwr_mean = squeeze(mean(pwr,3));
+
+%% Exact plotting code from new RHD matlab script
+imagesc(t/60, freqs, log10(pwr_mean + eps))
+ 
+hold on;
+axis xy
+ylabel('Frequency (Hz)')
+xlabel('Time (min)')
+h = colorbar;
+h.Label.String = 'Log(Power)';
+axis tight
+ylim([.5 55])
+caxis([0 1.5])
--- Intan/readRHDandDownsample.m
+++ Intan/readRHDandDownsample.m
@@ -1,30 +1,42 @@
-function [acc_pow,ephys_dec] = readRHDandDownsample(filename,shift,chunkSize)
+function [acc_pow,ephys_dec,dio] = readRHDandDownsample(filename,shift,chunkSize)
 
+    % extract current chunk of data
     fid = fopen(filename);
-    [ephys, acc, vdd, tmp, dio] = readRHD_oldFormat(fid, shift, chunkSize, 64);
+    [ephys, acc, ~, ~, dio] = readRHD_oldFormat(fid, shift, chunkSize, 64);
     fclose(fid);
 
+    % take the norm of accelerometer data
     acc_norm = vecnorm(acc);
 
+    % bandpass filter accelerometer data
     fc_acc = [.5 150];
-    fs_acc = size(acc,2)/chunkSize*30000;
-    [b_acc,a_acc] = butter(2,fc_acc/(fs_acc/2),'bandpass');
+    fs_acc = size(acc,2) / chunkSize * 30000;
+    [b_acc, a_acc] = butter(2, fc_acc / (fs_acc / 2), 'bandpass'); % 4th order
+    acc_filt = filter(b_acc ,a_acc, acc_norm);
 
-    acc_filt = filter(b_acc,a_acc,acc_norm);
-
+    %% extract 3-5Hz power of accelerometer data
     binsize = 1;
-    binsize_s = binsize*fs_acc;
+    binsize_s = binsize * fs_acc;
     acc_pow = [];
-    for i = 1:length(acc_norm)
-        if mod(i,binsize_s) == binsize_s/2 % centered
-            [pmt,fr] = pmtm(acc_filt(i-binsize_s/2+1:i+binsize_s/2),2,binsize_s,fs_acc);
-            acc_pow = [acc_pow; [i/fs_acc sum(pmt(iswithin(fr,3,5)))]];
+    
+    % sliding window to calculate 3-5Hz power
+    for w = 1 : length(acc_norm)
+        
+        % Get indices for current window
+        idx_start = w - binsize_s / 2 + 1;
+        idx_end = w + binsize_s / 2;
+        
+        % Get 3-5Hz power for window every binsize
+        if mod(w, binsize_s) == binsize_s / 2 % centered
+            [pwr,fr] = pmtm(acc_filt(idx_start : idx_end), 2, [], fs_acc);
+            acc_pow = [acc_pow; [w / fs_acc, sum(pwr(iswithin(fr, 3, 5)))]];
         end
     end
 
-    for i = 1:size(ephys,1)
-        tmp = ephys(1,:); ephys(1,:) = [];
+    %% downsample ephys data by 100x
+    for ele = 1:size(ephys,1)
+        tmp = ephys(ele,:);
         tmp = decimate(tmp,5,4);
         tmp = decimate(tmp,5,4);
-        ephys_dec(i,:) = decimate(tmp,4,4);
+        ephys_dec(ele,:) = decimate(tmp,4,4)*1e6; % convert to uV
     end
