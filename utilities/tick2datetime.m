657efa4b2c1f477396dd7b41a1130a2d4962e0f6
rudstre <31040517+rudstre@users.noreply.github.com>
2024-07-24 11:09:51 -0400

    updated tick2datetime so that it can handle strings

--- utilities/tick2datetime.m
+++ utilities/tick2datetime.m
@@ -1,3 +1,6 @@
 function dt = tick2datetime(ticks)
+if ~isnumeric(ticks)
+    ticks = str2num(ticks);
+end
 days_oi = ticks/(1e7*60*60*24);
 dt = datetime(days_oi + datenum(1,1,1),'ConvertFrom','datenum');
