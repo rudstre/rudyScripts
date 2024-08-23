e1393c026f7045692e74fd76f7e5244f4f06c4b1
rudstre <31040517+rudstre@users.noreply.github.com>
2024-08-23 12:14:19 -0400

    Fixed comments

--- OpenEphys/extractSignalChain.m
+++ OpenEphys/extractSignalChain.m
@@ -1,32 +1,34 @@
 function signalChain = extractSignalChain(filePath)
-% EXTRACTSIGNALCHAIN Extracts the signal processing chain from a JSON file.
+% EXTRACTSIGNALCHAIN Extracts the signal processing chain from a particular OpenEphys recording.
 %
-%   signalChain = EXTRACTSIGNALCHAIN(filePath) reads the specified JSON file,
-%   extracts the signal processing history for the first channel, and
-%   returns it as a cell array of steps in the signal chain.
+%   signalChain = EXTRACTSIGNALCHAIN(filePath) finds the structure.oebin file
+%   corresponding with the given path, extracts the signal processing 
+%   history for the first channel, and returns it as a cell array of steps 
+%   in the signal chain.
 %
 %   Inputs:
-%     - filePath: (Optional) A string specifying the path to the JSON file.
-%       If not provided, the function prompts the user to select a file.
+%     - filePath: (Optional) A string specifying the path to the directory
+%       containing the structure.oebin file. If not provided, the function prompts the
+%       user to select a file.
 %
 %   Output:
 %     - signalChain: A cell array where each element is a step in the signal
 %       processing chain.
 %
 %   Example:
-%     % Extract the signal chain from a specific JSON file:
-%     chain = extractSignalChain('path/to/file.json');
+%     % Extract the signal chain from Recording N:
+%     chain = extractSignalChain('path/to/recording/n');
 %
-%     % Prompt the user to select a JSON file and extract the signal chain:
+%     % Prompt the user to select a directory and extract the signal chain:
 %     chain = extractSignalChain();
 
-% If no file path is provided, prompt the user to select a file
+% If no file path is provided, prompt the user to select a directory
 if nargin == 0
     filePath = fileSelector();
 end
 
-% Read the contents of the JSON file into a string
-rawFile = fileread(fullfile(filePath,'structure.oebin'));
+% Read the contents of 'structure.oebin' into a string
+rawFile = fileread(fullfile(filePath, 'structure.oebin'));
 
 % Decode the JSON string into a MATLAB structure
 jsonStruct = jsondecode(rawFile);
